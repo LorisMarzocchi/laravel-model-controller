941010b3562e7ddc9aa1fafc62f17171a23758c2
LorisMarzocchi <ml77182a@gmail.com>
2023-06-26 17:43:16 +0200

    addImage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,14 +6,14 @@
 
     @foreach ($movies as $movie)
             <div class="card">
-                {{-- <img src="{{ Vite::asset('resources/img/' . $book->img) }}" class="card-img-top" alt=""> --}}
+                <img src="{{ Vite::asset('resources/img/' . $movie->img) }}" class="card-img-top" alt="">
                 <div class="card-body">
                     <h3>{{ $movie->title }}</h3>
-                <h5 class="card-title">{{ $movie->original_title }} </h5>
-                <h5 class="card-title">{{ $movie->nationality }} </h5>
-                <h5 class="card-title">{{ $movie->date }} </h5>
-                <h5 class="card-title">{{ $movie->vote }} </h5>
-                </div>
+                    <h5 class="card-title">{{ $movie->original_title }} </h5>
+                    <h5 class="card-title">{{ $movie->nationality }} </h5>
+                    <h5 class="card-title">{{ $movie->date }} </h5>
+                    <h5 class="card-title">{{ $movie->vote }} </h5>
+                 </div>
             </div>
     @endforeach
 </div>
